86d268d09caf5c8daed01caf928e9f138c11d158
gabisilvano <gabrielle.silvano@gmail.com>
2018-07-12 17:48:45 +0200

    first commit

--- site_01.rb
+++ site_01.rb
@@ -1,16 +1,32 @@
 require 'open-uri'
 require 'nokogiri'
 require 'pry'
-
-
-def get_the_email_of_a_townhal_from_its_webpage (site)
-	mail = []
-	page = Nokogiri::HTML(open(site))
-	page.xpath("//td").each do |node|
-    mail << node.text if node.text.include?("@")
+def get_the_email_of_a_townhal_from_its_webpage(site)
+  mail = []
+  page = Nokogiri::HTML(open(site))
+  page.xpath('//td').each do |node|
+    mail << node.text if node.text.include?('@')
   end
-	puts mail
-	#items = page.xpath("//div/a/h4").collect {|node| puts node.text.strip}	
+  puts mail
+	
 end
 
-get_the_email_of_a_townhal_from_its_webpage ("http://annuaire-des-mairies.com/95/vaureal.html")
+def get_all_the_urls_of_val_doise_townhalls(link, city)
+
+  site_1 = ''
+  city_name = []
+  city_link = ''
+  Nokogiri::HTML(open(link)).xpath('//a').each do |node|
+    site_1 = link + node.values[1] if node.text.include?(city)
+  end
+  site_2 = Nokogiri::HTML(open(site_1))
+  site_2.xpath('//a[@class]').each do |node|
+    city_name << node.values[1] if node.values[1][0].include?('.')
+  end
+  city_name.each do |a|
+    link_3 = link + a[1..a.length - 1]
+    get_the_email_of_a_townhal_from_its_webpage link_3
+  end
+end
+
+get_all_the_urls_of_val_doise_townhalls('http://annuaire-des-mairies.com/', "95 | Val-d'Oise")
